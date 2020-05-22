12f5e422dc54858460254334fc66276d8a1b1d0e
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-22 14:26:45 +0200

    Fix version management

--- scripts/generate-and-validate-doc.sh
+++ scripts/generate-and-validate-doc.sh
@@ -1,15 +1,21 @@
-SHORT_VERSION=$(echo $version | cut -d. -f1-2)
+#!/bin/bash
+
+set -e
+. $BASEDIR/arrow-master/scripts/commons4gradle.sh
+
+echo "For version: $VERSION ..."
+SHORT_VERSION=$(echo $VERSION | cut -d. -f1-2)
 
 cd $BASEDIR/arrow-site
 git checkout .
 cp sidebar/$SHORT_VERSION/* docs/_data/
-sed -i "s/latest/$version/g" docs/_includes/_head-docs.html
+sed -i "s/latest/$VERSION/g" docs/_includes/_head-docs.html
 ./gradlew clean runAnk
 
 cd $BASEDIR/arrow
 git checkout .
-git checkout $version
-sed -i "s/^VERSION_NAME.*/VERSION_NAME=$version/g" gradle.properties
+git checkout $VERSION
+sed -i "s/^VERSION_NAME.*/VERSION_NAME=$VERSION/g" gradle.properties
 replaceOSSbyBintrayRepository generic-conf.gradle
 
 # TODO: Remove when releasing 0.11.0
@@ -18,7 +24,7 @@ cp $BASEDIR/arrow-master/doc-conf.gradle $BASEDIR/arrow/
 for repository in $(cat $BASEDIR/arrow/lists/libs.txt); do
     cd $BASEDIR/$repository
     git checkout .
-    git checkout $version
+    git checkout $VERSION
     replaceGlobalPropertiesbyLocalConf gradle.properties
     if [ -f arrow-docs/build.gradle ]; then
         replaceOSSbyBintrayRepository arrow-docs/build.gradle
