e784d2c659d3263b335ccbde34154f1c8924d336
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-08-10 12:26:58 +0200

    Configuration: remove unused directory

--- scripts/generate-and-validate-doc.sh
+++ scripts/generate-and-validate-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-. $BASEDIR/arrow-master/scripts/commons4gradle.sh
+. $BASEDIR/arrow/scripts/commons4gradle.sh
 
 echo "For version: $VERSION ..."
 SHORT_VERSION=$(echo $VERSION | cut -d. -f1-2)
@@ -18,9 +18,6 @@ git checkout $VERSION
 perl -pe "s/^VERSION_NAME.*/VERSION_NAME=$VERSION/g" -i gradle.properties
 replaceOSSbyBintrayRepository generic-conf.gradle
 
-# TODO: Remove when releasing 0.11.0
-cp $BASEDIR/arrow-master/doc-conf.gradle $BASEDIR/arrow/
-
 for repository in $(cat $BASEDIR/arrow/lists/libs.txt); do
     cd $BASEDIR/$repository
     git checkout .
@@ -30,8 +27,8 @@ for repository in $(cat $BASEDIR/arrow/lists/libs.txt); do
         replaceOSSbyBintrayRepository arrow-docs/build.gradle
     fi
     addArrowDocs $BASEDIR/$repository/settings.gradle
-    $BASEDIR/arrow-master/scripts/project-assemble.sh $repository
-    $BASEDIR/arrow-master/scripts/project-run-dokka.sh $repository
-    $BASEDIR/arrow-master/scripts/project-run-ank.sh $repository
-    $BASEDIR/arrow-master/scripts/project-locate-doc.sh $repository
+    $BASEDIR/arrow/scripts/project-assemble.sh $repository
+    $BASEDIR/arrow/scripts/project-run-dokka.sh $repository
+    $BASEDIR/arrow/scripts/project-run-ank.sh $repository
+    $BASEDIR/arrow/scripts/project-locate-doc.sh $repository
 done
