148d2d926f29bbf27cd02c50ffaee9709bd6584c
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-08-27 10:35:38 +0200

    Move load functions and note to refactor

--- scripts/generate-and-validate-doc.sh
+++ scripts/generate-and-validate-doc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-. $BASEDIR/arrow-master/scripts/commons4gradle.sh
 
 echo "For version: $VERSION ..."
 SHORT_VERSION=$(echo $VERSION | cut -d. -f1-2)
@@ -15,12 +14,15 @@ perl -pe "s/latest/$VERSION/g" -i docs/_includes/_head-docs.html
 cd $BASEDIR/arrow
 git checkout .
 git checkout $VERSION
+. ./scripts/commons4gradle.sh
 perl -pe "s/^VERSION_NAME.*/VERSION_NAME=$VERSION/g" -i gradle.properties
-replaceOSSbyBintrayRepository generic-conf.gradle
+replaceOSSbyBintrayRepository "*.gradle"
+replaceOSSbyBintrayRepository "gradle/*.gradle"
 
 # TODO: Remove when releasing 0.11.0
 cp $BASEDIR/arrow-master/gradle/apidoc-creation.gradle $BASEDIR/arrow/doc-conf.gradle
 
+# TODO: Refactor when releasing 0.11.0
 for repository in $(cat $BASEDIR/arrow/lists/libs.txt); do
     cd $BASEDIR/$repository
     git checkout .
