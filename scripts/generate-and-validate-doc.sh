c1aa4c6bcd694cbbc55c6a3c8c0509ea0a12fdb6
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-10-08 10:40:31 +0200

    Adaptation

--- scripts/generate-and-validate-doc.sh
+++ scripts/generate-and-validate-doc.sh
@@ -7,8 +7,9 @@ echo "For version: $VERSION ..."
 
 cd $BASEDIR/arrow-site
 git checkout .
+git clean -dxf -e vendor -e .bundle
 if [ -d $BASEDIR/arrow-site-$VERSION ]; then cp $BASEDIR/arrow-site-$VERSION/docs/_data/sidebar* docs/_data/; fi
-perl -pe "s/latest/$VERSION/g" -i docs/_includes/_head-docs.html
+perl -pe "s/latest/$VERSION/g" -i docs/_includes/_head-docs.html # TODO
 
 cd $BASEDIR/arrow
 git checkout .
--- scripts/publish-latest-release.sh
+++ scripts/publish-latest-release.sh
@@ -5,19 +5,35 @@
 
 set -e
 
+MAIN_CONTENT=("CNAME"  "code"  "css"  "error.html"  "fonts"  "img"  "index.html"  "js"  "redirects.json")
+
 cd _site/
 for file in *; do
-    if [ -f "$file" ]; then
-        echo "Copying $file ..."
-        echo "aws s3 cp $file s3://$S3_BUCKET/$file" >> $BASEDIR/logs/aws_sync.log
-        #aws s3 cp $file s3://$S3_BUCKET/$file >> $BASEDIR/logs/aws_sync.log
-        continue
-    fi
-    if [ "$file" != "docs" ]; then
-        echo "Sync $file ..."
+
+    # Landscape and related
+    #
+    if [[ ${MAIN_CONTENT[*]} =~ "$file" ]]; then
+        if [ -f "$file" ]; then
+            echo "Copying main content: $file ..."
+            echo "aws s3 cp $file s3://$S3_BUCKET/$file" >> $BASEDIR/logs/aws_sync.log
+            #aws s3 cp $file s3://$S3_BUCKET/$file >> $BASEDIR/logs/aws_sync.log
+            continue
+        fi
+        echo "Sync main content: $file ..."
         echo "aws s3 sync $file s3://$S3_BUCKET/$file --delete" >> $BASEDIR/logs/aws_sync.log
         #aws s3 sync $file s3://$S3_BUCKET/$file --delete >> $BASEDIR/logs/aws_sync.log
         continue
     fi
-    echo ">>>>>>>>>> $file"
+
+    # /docs/
+    #
+    if [ -f "$file" ]; then
+        echo "Copying $file ..."
+        echo "aws s3 cp $file s3://$S3_BUCKET/docs/$file" >> $BASEDIR/logs/aws_sync.log
+        #aws s3 cp $file s3://$S3_BUCKET/docs/$file >> $BASEDIR/logs/aws_sync.log
+        continue
+    fi
+    echo "Sync $file ..."
+    echo "aws s3 sync $file s3://$S3_BUCKET/docs/$file --delete" >> $BASEDIR/logs/aws_sync.log
+    #aws s3 sync $file s3://$S3_BUCKET/docs/$file --delete >> $BASEDIR/logs/aws_sync.log
 done
